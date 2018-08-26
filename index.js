c9ff42ae506fb63ae245c4afa1a626c4896d3a8b
David masterson <david73180@gmail.com>
2018-08-26 01:57:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,10 +11,10 @@ function logWhisper() {
   console.log("hello")
 }
 function sayHiToGrandma(string) {
-    if (string.toLowerCase === string) {
+    if (string === string.toLowerCase) {
       return  'I can\'t hear you!'
     }
-    if (string.toUpperCase === string) {
+    if (string === string.toUpperCase) {
       return  'YES INDEED!'
     }
     if (string === 'I love you, Grandma.') {
