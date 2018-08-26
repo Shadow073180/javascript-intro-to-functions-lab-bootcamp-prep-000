ebf3cc9fd248d0862e22435cfce61a79b48a27da
David masterson <david73180@gmail.com>
2018-08-26 01:23:40 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,15 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(string) {
+  console.log("HELLO")
+}
+function logWhisper() {
+  console.log("hello")
+}
+function sayHiToGrandma(string) {
+  return ("I can't hear you") if 
+  sayHiToGrandma.toLowerCase===sayHiToGrandma
